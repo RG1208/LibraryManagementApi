6509ff56d2282dd7f98b8ba87070c61e57220fde
Rachit Garg <rachitgarg1208@gmail.com>
2024-07-23 16:59:22 +0530

    feat: Access And Refresh Token Utils

---
+++ src/utils/AccessRefreshToken.js
@@ -0,0 +1,23 @@
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
+
+
+const generateAccessAndRefreshToken = async (userId) => {
+
+    try {
+
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+        return { accessToken, refreshToken }
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating access and refresh tokens")
+    }
+
+}
+
+export { generateAccessAndRefreshToken }
