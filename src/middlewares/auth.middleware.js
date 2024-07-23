98d55c82782133892b416ce67e4ae9ead217e0b7
Rachit Garg <rachitgarg1208@gmail.com>
2024-07-23 21:57:46 +0530

    refactor: Getting User profile

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,7 +1,7 @@
 import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import jwt from "jsonwebtoken"
-import { User } from "../models/user.model.js";
+import { User } from "../models/users.models.js";
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
@@ -10,7 +10,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
 
         // console.log(token);
         if (!token) {
-            throw new ApiError(401, "Unauthorized request")
+            throw new ApiError(401, "Unauthorized request received")
         }
 
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
@@ -7,6 +8,9 @@ router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
 
 // secured Routes
-router.route("/logout").post(logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser)
 
+router.route("/me").get(verifyJWT, async (req, res) => {
+    res.send(req.user);
+})
 export default router
