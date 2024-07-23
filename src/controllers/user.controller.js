3c8f913bceb1cca26524cddd718f00f2103672f9
Rachit Garg <rachitgarg1208@gmail.com>
2024-07-23 21:22:07 +0530

    refactor: Logout controller and route updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -128,4 +128,30 @@ const loginUser = asyncHandler(async (req, res) => {
 
 })
 
-export { registerUser, loginUser }
+// LOGOUT METHOD
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                refreshToken: undefined// this removes the field from document
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "User logged Out Successfully"))
+})
+
+export { registerUser, loginUser, logoutUser }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { registerUser, loginUser } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
 
 const router = Router()
 
 router.route("/register").post(registerUser)
 router.route("/login").post(loginUser)
+
+// secured Routes
+router.route("/logout").post(logoutUser)
 
 export default router
