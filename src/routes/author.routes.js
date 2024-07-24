e7fcd0614e43af8b06a18a570e5c9617282d96a7
Rachit Garg <rachitgarg1208@gmail.com>
2024-07-24 23:50:23 +0530

    fix: admin access updated

--- src/routes/author.routes.js
+++ src/routes/author.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { registerAuthor, getAuthors, getUserProfile, updateAuthorProfile, deleteAuthor } from "../controllers/author.controller.js";
-
+import { adminMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
-router.route("/register").post(registerAuthor)
+router.route("/register").post(adminMiddleware, registerAuthor)
 router.route("/getAuthors").get(getAuthors)
 router.route("/:id").get(getUserProfile)
-router.route("/:id").put(updateAuthorProfile)
-router.route("/:id").delete(deleteAuthor)
+router.route("/:id").put(adminMiddleware, updateAuthorProfile)
+router.route("/:id").delete(adminMiddleware, deleteAuthor)
 export default router
--- src/routes/books.routes.js
+++ src/routes/books.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import { createBook, getBooks, getBookProfile, updateBook, deleteBook } from "../controllers/books.controllers.js";
-
+import { adminMiddleware } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
-router.route("/register").post(createBook)
+router.route("/register").post(adminMiddleware, createBook)
 router.route("/getBooks").get(getBooks)
 router.route("/:id").get(getBookProfile)
-router.route("/:id").put(updateBook)
-router.route("/:id").delete(deleteBook)
+router.route("/:id").put(adminMiddleware, updateBook)
+router.route("/:id").delete(adminMiddleware, deleteBook)
 
 export default router
